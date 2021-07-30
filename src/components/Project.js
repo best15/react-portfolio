9a60b509a6ea4bd26325fc80bd31eb6b76dfa4cb
SBC <lochan.sharad@gmail.com>
2021-07-31 00:18:53 +0800

    style added for portfolio pages

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,16 +1,22 @@
 import React, { Fragment } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
-function Project({ deploylink, projectImage }) {
+function Project({ deployLink, projectImage, title }) {
 
   return (
     <Fragment>
-      <div className=" col-sm-12 col-md-6 col-lg-5 mb-3  ">
+      <div className=" col-sm-12 col-md-6 col-lg-5 mb-3  projectContainer">
 
-        <a href={deploylink} target="_blank">
-          <img id="img" src={projectImage} alt="Fullstack I CAN HELP application " />
+        <img id="img" src={projectImage} alt="Fullstack I CAN HELP application " />
 
-        </a>
+        <div className="links">
+          <a href={deployLink} target="_blank">
+            <h2 className="title">{title}</h2>
+          </a>
+          <a href={deployLink} target="_blank"><FontAwesomeIcon icon={faGithub} /> </a>
+        </div>
 
       </div>
     </Fragment>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,6 +5,7 @@ import iCanHelpImage from "../../assets/images/i-can-help.jpg"
 import weatherForecastImage from "../../assets/images/weather-forecast.jpg"
 import dayPlanerImage from "../../assets/images/day-planner.jpg"
 import codequizImage from "../../assets/images/codequiz.jpg"
+import workoutTrackerImage from "../../assets/images/workout-tracker.jpg"
 
 
 function Portfolio() {
@@ -12,27 +13,45 @@ function Portfolio() {
     const projectValues = [
         {
             deployLink: 'https://dudley-mountie-64122.herokuapp.com/',
-            projectImage: techBlogImage
+            projectImage: techBlogImage,
+            title: "Tech Blog",
+            repoLink: "https://github.com/best15/tech-blog"
         },
         {
-            deployLink: 'https://dudley-mountie-64122.herokuapp.com/',
-            projectImage: iCanHelpImage
+            deployLink: 'https://i-can-help-work-market-place.herokuapp.com/',
+            projectImage: iCanHelpImage,
+            title: "I_CAN_HELP",
+            repoLink: "https://github.com/Twistedmouse/I-CAN-HELP-WorkMarketPlace"
         },
         {
-            deployLink: 'https://dudley-mountie-64122.herokuapp.com/',
-            projectImage: weatherForecastImage
+            deployLink: 'https://workout-tracker-app-21.herokuapp.com/',
+            projectImage: workoutTrackerImage,
+            title: "Workout Tracker",
+            repoLink: "https://github.com/best15/workout-tracker"
         },
         {
-            deployLink: 'https://dudley-mountie-64122.herokuapp.com/',
-            projectImage: codequizImage
+            deployLink: 'https://best15.github.io/weather-forecast/',
+            projectImage: weatherForecastImage,
+            title: "Weather Forecast",
+            repoLink: "https://github.com/best15/weather-forecast"
         },
         {
-            deployLink: 'https://dudley-mountie-64122.herokuapp.com/',
-            projectImage: dayPlanerImage
+            deployLink: 'https://best15.github.io/codequiz/',
+            projectImage: codequizImage,
+            title: "Code Quiz",
+            repoLink: "https://github.com/best15/codequiz"
+        },
+        {
+            deployLink: 'https://best15.github.io/day-planner/',
+            projectImage: dayPlanerImage,
+            title: "Day Planner",
+            repoLink: "https://github.com/best15/day-planner"
         },
     ]
 
-    const projectMap = projectValues.map(el => <Project deployLink={el.deployLink} projectImage={el.projectImage} />)
+    const projectMap = projectValues.map(el =>
+        <Project deployLink={el.deployLink} projectImage={el.projectImage} title={el.title} />
+    );
 
 
 
